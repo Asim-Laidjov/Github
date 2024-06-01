8b309044c0eafb9b16f7c61f6e77b947ef7b7f94
Asim-Laidjov <24asimlaidjov@gmail.com>
2024-06-01 13:20:34 +0400

    portfolio creator js

---
+++ index.js
@@ -0,0 +1,15 @@
+let proyektSwiper = new Swiper(".projectsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+   // pagination: {
+      //el: ".swiper-pagination",
+      //clickable: true,
+  //  },
+
+    navigation: {
+        nextEl: ".evvel",
+        prevEl: ".sonra",
+      },
+    
+  });
+
