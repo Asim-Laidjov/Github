55bc335f216edf3f7e305a9c2331304868d682a5
Asim-Laidjov <24asimlaidjov@gmail.com>
2024-06-09 13:16:54 +0400

    portfolio-all

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let proyektSwiper = new Swiper(".projectsSwiper", {
+let proyektSwiper = new Swiper(".projectSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
    // pagination: {
@@ -7,9 +7,82 @@ let proyektSwiper = new Swiper(".projectsSwiper", {
   //  },
 
     navigation: {
-        nextEl: ".evvel",
-        prevEl: ".sonra",
+        nextEl: ".sonra",
+        prevEl: ".evvel",
       },
     
   });
+
+ 
+
+  // bloglar 
+
+  window.addEventListener("DOMContentLoaded",getMelumatlariGetir) 
+
+  const bloqlarinYerleseceyiDiv = document.querySelector(".blog-right-side")
+  console.log(bloqlarinYerleseceyiDiv)
+  window.addEventListener("DOMContentLoaded", getMelumatlariGetir)
+
+  async function getMelumatlariGetir(){
+    const unvan = "https://jsonplaceholder.typicode.com/posts"
+
+
+
+    try {
+      const gelenCavab= await fetch(unvan)
+      const melumatlar=await gelenCavab.json()
+
+      console.table(melumatlar)
+
+    
+  
+  for(let i = 0; i < melumatlar.length; i++) {
+    bloqlarinYerleseceyiDiv.innerHTML+= ` <div class="blog py-3">
+    <span><span id="date">April 16, 2021 </span><span id="time"> 6 mins</span></span>
+    <h2>${melumatlar[i].title}</h2>
+    <a href="" class="text-white">Read the article <img src="./assets/images/blog.svg" alt=""></a>
+  </div> `
+
+  }
+}
+ catch (err) {
+  
+ }
+}
+
+
+const swiper = new Swiper(".test-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 50,
+    },
+
+  },
+
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+ },
+  navigation: {
+    nextEl: ".nextButton",
+    prevEl: ".prevButton",
+  }
+});
+
+// OOP
+new Accordion('.accordion-container');
+new Accordion('.accordion-container-2');
 
